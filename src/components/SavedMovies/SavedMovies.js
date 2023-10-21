a643b71809d48b49750fb2ce12870a3cb770fb45
ValentinaNov <novoseletskayav@gmail.com>
2023-10-21 05:11:51 +0300

    fix savedmovies search

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -5,13 +5,15 @@ import { filterByKeyword, filterByIsShortMovies } from '../../utils/utils';
 
 function SavedMovies({savedMovies, handleDeleteMovie}) { 
     const isSavedPage = true;
- 
+    
+    const [queryWord, setQueryWord] = useState(''); 
     const [isShortMovies, setIsShortMovies] = useState(false); 
     const [searchResults, setSearchResults] = useState([]); 
     const [displayMovies, setDisplayMovies] = useState([]);  
 
     const handleInputChange = useCallback( (keyword) => {   
         if (keyword !==''){ 
+            setQueryWord(keyword);
             const results = filterByKeyword(savedMovies, keyword); 
             setSearchResults(results);  
         } else { 
@@ -23,7 +25,16 @@ function SavedMovies({savedMovies, handleDeleteMovie}) {
         const newState = !isShortMovies;
         setIsShortMovies(newState); 
     }
- 
+    
+    useEffect(() => { 
+        if (queryWord !==''){  
+            const results = filterByKeyword(savedMovies, queryWord); 
+            setSearchResults(results);  
+        } else { 
+            setSearchResults(savedMovies);  
+        }
+    }, [savedMovies, queryWord]);
+
     useEffect(() => { 
         if (searchResults.length !== 0) {
             if (isShortMovies) {
@@ -33,29 +44,14 @@ function SavedMovies({savedMovies, handleDeleteMovie}) {
                 setDisplayMovies(searchResults);
             }
         } else {
-            if (isShortMovies) {
-                const newMovies = filterByIsShortMovies(savedMovies, setIsShortMovies);
-                setDisplayMovies(newMovies);
-            } else {
-                setDisplayMovies(savedMovies);
-            }
-            
+            setDisplayMovies([]);
         } 
-
-         
-    }, [savedMovies, searchResults, isShortMovies]);
-
- 
- 
-  
+    }, [searchResults, isShortMovies, queryWord]);
       
     return (
         <main>
-             
             <Search isShortMovies={isShortMovies} handleShortChange={handleShortChange} handleInputChange={handleInputChange} showOnlySaved={isSavedPage} />
-             
             <MoviesCardList displayMovies={displayMovies} handleDeleteMovie={handleDeleteMovie} showOnlySaved={isSavedPage}/>
-       
         </main>
     );
 }
